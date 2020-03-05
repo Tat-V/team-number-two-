ba8509290e4630c255ba0f708db6cba215555a21
Poshushukaemsya <61202923+Poshushukaemsya@users.noreply.github.com>
2020-03-05 20:27:17 +0300

    The same code
    Uploading the same code to exclude sad emptiness from this repository

---
+++ setup.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+TOKEN = '1115589006:AAGh_WvlwZpdnyt4id-EbltJvaFqYH635dk'  # insert your token here
+PROXY = 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
