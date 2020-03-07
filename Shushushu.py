92d04c7d45123a1ea4df5fe66b12142819a35777
Poshushukaemsya <61202923+Poshushukaemsya@users.noreply.github.com>
2020-03-07 17:52:21 +0300

    update
    homework done

--- Shushushu.py
+++ Shushushu.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import logging
-
+from functools import wraps
 from setup import PROXY, TOKEN
 from telegram import Bot, Update
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
+
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,25 +17,79 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 
+LOG_ACTIONS = []
 
+
+
+
+
+
+def log_action(function):
+    def inner(*args, **kwargs):
+        update = args[0]
+        if update and hasattr(update, 'message') and hasattr(update, 'effective_user'):
+            LOG_ACTIONS.append({
+            'user': update.effective_user.first_name,
+            'user id': update.message.chat.id,
+            'function': function.__name__,
+            'message': update.message.text, })
+            with open("History.txt", "a", encoding="UTF-8") as file_h:
+                for key, value in LOG_ACTIONS[-1].items():
+                    file_h.write(key + ':' + (str(value)) + "\t")
+                file_h.write("\n")
+        return function(*args, **kwargs)
+    return inner
+
+
+def decorator_error(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as er:
+            print("Error! " + str(er) + ', ' + "function: " + func.__name__)
+    return wrapper
+
+
+@log_action
+@decorator_error
 def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
-    update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
+    update.message.reply_text(f'Привет, {update.effective_user.first_name}!', print(gasga))
 
 
+@log_action
+@decorator_error
 def chat_help(update: Update, context: CallbackContext):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Введи команду /start для начала. ')
 
 
+@log_action
 def echo(update: Update, context: CallbackContext):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
-
+@log_action
 def error(update: Update, context: CallbackContext):
     """Log Errors caused by Updates."""
     logger.warning(f'Update {update} caused error {context.error}')
+
+
+@log_action
+@decorator_error
+def history(update: Update, context: CallbackContext):
+    """Send user last 5 records from history."""
+    # file_hist = open("History.txt", "r", encoding="UTF-8")
+    hist = ""
+    with open("History.txt", "r", encoding="UTF-8") as file_h:
+        hist_all = file_h.readlines()
+        if len(hist_all) > 4:
+            for i in range(-1, -6, -1):
+                hist += hist_all[i]
+        else:
+            hist = "\t".join(hist_all)
+        update.message.reply_text(hist)
 
 
 def main():
@@ -47,6 +102,7 @@ def main():
     # on different commands - answer in Telegram
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', chat_help))
+    updater.dispatcher.add_handler(CommandHandler('history', history))
 
     # on noncommand i.e message - echo the message on Telegram
     updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
