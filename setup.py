011f1a52a1ff9c886e261eebcb09a6bea58808db
Poshushukaemsya <61202923+Poshushukaemsya@users.noreply.github.com>
2020-05-12 19:18:03 +0300

    Deleting old files due to global refactoring

--- setup.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-TOKEN = '1115589006:AAGh_WvlwZpdnyt4id-EbltJvaFqYH635dk'  # insert your token here
-PROXY = 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
-TOKEN_WEATHER = 'd081cc3b0aa6fbb8e8f24d1a8216be49' # # insert your token weather here
